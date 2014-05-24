160cde729fc58ebfb58ee5a049471840c39f3245
James Lefrère <jlefrere@gmail.com>
2014-05-25 00:54:05 +0100

    Added connections and selected helpers to game template. Calculates distance to nearby beacons and draws connections.

--- client/views/game.js
+++ client/views/game.js
@@ -1,5 +1,20 @@
 Template.game.helpers({
-
+  connections: function () {
+    if (typeof Session.get('currentBeacon') !== 'undefined' && typeof this.beacons !== 'undefined') {
+      var beacons = [];
+      var currentBeacon = this.beacons[Session.get('currentBeacon')];
+      _.each(this.beacons, function (beacon) {
+        if (Math.pow(beacon.x - currentBeacon.x, 2) + Math.pow(beacon.y - currentBeacon.y, 2) <= 200*200)
+          beacons.push({ cx: currentBeacon.x, cy: currentBeacon.y, x: beacon.x, y: beacon.y });
+      });
+      return beacons;
+    }
+  },
+  selected: function () {
+    if (typeof Session.get('currentBeacon') !== 'undefined') {
+      return Session.get('currentBeacon') === this.id;
+    }
+  }
 });
 
 Template.game.events({
