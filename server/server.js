03e7ecd1cd460924d9d397fb3743acbd92d62740
James Lefrère <jlefrere@gmail.com>
2014-05-24 22:47:31 +0100

    Changed createGame method to use PRNG to set 16 beacons for the game with pseudorandom x and y coords based on the gamefield width and height

--- server/server.js
+++ server/server.js
@@ -7,16 +7,29 @@ Meteor.methods({
     if (typeof data.seed !== 'string' || typeof data !== 'object')
       throw new Meteor.Error(400, 'No data received');
 
-    self.seed = SeedRandom(data.seed);
+    self.rng = SeedRandom(data.seed);
+    self.seedValue = self.rng();
+    self.seedString = data.seed;
+    self.slug = _.slugify(self.seedString);
     self.time = (new Date()).getTime();
-    self.slug = _.slugify(data.seed);
+
+    self.beacons = [];
+    self.gameWidth = 800;
+    self.gameHeight = 600;
+
+    for (var i = 0; i < 16; i++) {
+      var x = self.rng() * self.gameWidth;
+      var y = self.rng() * self.gameHeight;
+      self.beacons.push({x: x, y: y});
+    }
 
     var game = Games.insert({
       creator: self.userId,
-      seedString: data.seed,
-      seedValue: self.seed(),
+      seedValue: self.seedValue,
+      seedString: self.seedString,
+      slug: self.slug,
       time: self.time,
-      slug: self.slug
+      beacons: self.beacons
     });
 
     if (typeof game !== 'undefined') {
