0643e3ffdfb823746bd6f1f4c243476c82ae1ad1
James Lefrère <jlefrere@gmail.com>
2014-05-24 20:55:37 +0100

    Games collection and basic createGame method

--- server/server.js
+++ server/server.js
@@ -1,4 +1,24 @@
 SeedRandom = Meteor.require('seedrandom');
 
-//var rng = SeedRandom('hello.');
-//console.log(rng());
+Meteor.methods({
+  'createGame': function (data) {
+    var self = this;
+
+    if (data.seed !== 'string' || typeof data !== 'object')
+      throw Meteor.Error(400, 'No data received');
+
+    if (data.seed !== 'undefined') {
+      self.seed = SeedRandom(data.seed);
+    } else {
+      self.seed = SeedRandom();
+    }
+
+    self.time = (new Date()).getTime();
+
+    Games.insert({
+      creator: self.userId,
+      seed: self.seed,
+      time: self.time
+    });
+  }
+});
