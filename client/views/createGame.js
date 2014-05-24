3cd322a703e0b413a0522eae797aea46b31d7a43
James Lefrère <jlefrere@gmail.com>
2014-05-24 21:11:57 +0100

    Added createGame template

---
+++ client/views/createGame.js
@@ -0,0 +1,17 @@
+Template.createGame.helpers({
+  randomString: function () {
+    return Math.random().toString(36).substring(8);
+  }
+});
+
+Template.createGame.events({
+  'submit #createGame': function (e, t) {
+    e.preventDefault();
+    var seed = t.$('#createGameSeed').val();
+    Meteor.call('createGame', { seed: seed }, function (err, res) {
+      if (!err) {
+        console.log(res);
+      } else console.log(err);
+    });
+  }
+});
