467904e93fb2ef7f8ad47205672a8e13929fb270
James Lefrère <jlefrere@gmail.com>
2014-05-25 16:00:13 +0100

    Prevent SVG from panning past extents

--- client/views/game.js
+++ client/views/game.js
@@ -22,3 +22,31 @@ Template.game.events({
     Session.set('currentBeacon', $(e.currentTarget).data('id'));
   }
 });
+
+Template.game.rendered = function () {
+  var self = this;
+  self.$game = $('#game');
+  self.gameWidth = self.$game.data('width');
+  self.gameHeight = self.$game.data('height');
+
+  var panZoom = svgPanZoom('#game', {
+    zoomEnabled: false,
+    minZoom: 1,
+    maxZoom: 1
+  });
+  panZoom.setOnPan(function () {
+    var gutter = 100;
+    var maxRight = self.gameWidth - gutter;
+    var maxLeft = -self.gameWidth + gutter;
+    var maxTop = self.gameHeight - gutter;
+    var maxBottom = -self.gameHeight + gutter;
+    if (this.getPan().x > maxRight)
+      this.pan({x: maxRight, y: this.getPan().y});
+    if (this.getPan().x < maxLeft)
+      this.pan({x: maxLeft, y: this.getPan().y});
+    if (this.getPan().y > maxTop)
+      this.pan({x: this.getPan().x, y: maxTop});
+    if (this.getPan().y < maxBottom)
+      this.pan({x: this.getPan().x, y: maxBottom});
+  });
+};
