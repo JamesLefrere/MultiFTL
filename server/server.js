4c8155fe9509d0111287bca69800f78115cb0b23
James Lefrère <jlefrere@gmail.com>
2014-05-25 16:29:23 +0100

    NameGenerator with PRNG

--- server/server.js
+++ server/server.js
@@ -1,5 +1,48 @@
 SeedRandom = Meteor.require('seedrandom');
 
+var phonemes = {};
+var greek = {};
+phonemes = JSON.parse(Assets.getText('phonemes.json'));
+greek = JSON.parse(Assets.getText('greek.json'));
+
+var NameGenerator = function (rng, category) {
+  if (typeof rng !== 'number' || typeof category !== 'string')
+    throw new Meteor.Error(500, 'Could not generate name');
+
+  var self = this;
+  self.rng = new SeedRandom(rng);
+  self.name = '';
+
+  self.getFragment = function (options) {
+    var fragment = '';
+    for (i = 0; i < options.segments; i++) {
+      var corpusLength = options.corpus.length;
+      var randomEntry = Math.round(self.rng() * corpusLength);
+      if (randomEntry > corpusLength)
+        randomEntry--;
+      fragment += options.corpus[randomEntry];
+    }
+    if (options.titleCase)
+      fragment = _.str.capitalize(fragment);
+    if (options.separator)
+      fragment += options.separator;
+    self.name += fragment;
+  };
+
+  self.getName = function () {
+    return self.name;
+  };
+
+  switch (category) {
+    case 'beacon':
+      self.getFragment({ segments: 2, corpus: phonemes, titleCase: true, separator: ' ' });
+      self.getFragment({ segments: 1, corpus: greek });
+      break;
+  }
+
+
+};
+
 Meteor.methods({
   'createGame': function (data) {
     var self = this;
@@ -7,7 +50,7 @@ Meteor.methods({
     if (typeof data.seed !== 'string' || typeof data !== 'object')
       throw new Meteor.Error(400, 'No data received');
 
-    self.rng = SeedRandom(data.seed);
+    self.rng = new SeedRandom(data.seed);
     self.seedValue = self.rng();
     self.seedString = data.seed;
     self.slug = _.slugify(self.seedString);
@@ -20,7 +63,8 @@ Meteor.methods({
     for (var i = 0; i < 72; i++) {
       var x = self.rng() * self.gameWidth;
       var y = self.rng() * self.gameHeight;
-      self.beacons.push({id: i, x: x, y: y});
+      var name = new NameGenerator(self.rng(), 'beacon');
+      self.beacons.push({id: i, name: name.getName(), x: x, y: y});
     }
 
     var game = Games.insert({
